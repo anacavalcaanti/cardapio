ca000c57b8f09ba7e487e75e02958e2bc126f340
ana1c <ana1carolinacosta@gmail.com>
2023-11-19 03:25:33 -0300

    feat: conectando banco de dados postgres

---
+++ cardapio/src/main/java/fullstack/com/br/cardapio/adapter/controller/ComidaController.java
@@ -0,0 +1,14 @@
+package fullstack.com.br.cardapio.core.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("comida")
+public class ComidaController {
+
+
+    public void getAll(){
+
+    }
+}
